1e603d13bfdfb45732b004c2ec3cd9addaad480c
Momenkhk <vini24momen@gmail.com>
2026-03-04 23:14:32 +0200

    Delete events directory

--- events/messageUpdate.js
+++
@@ -1,2 +0,0 @@
-const logEvent = require('../systems/loggingSystem');
-module.exports = { name: 'messageUpdate', async execute(client, oldMessage, newMessage) { if (newMessage.guild && oldMessage.content !== newMessage.content) await logEvent(newMessage.guild, 'Message Edit', `Before: ${oldMessage.content || 'N/A'}\nAfter: ${newMessage.content || 'N/A'}`); } };
