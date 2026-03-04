1ba41e93755c3368d4308b59cedcd1736bca8c7b
Momenkhk <vini24momen@gmail.com>
2026-03-04 23:50:51 +0200

    Delete events directory

--- events/messageDeleteBulk.js
+++
@@ -1,2 +0,0 @@
-const logEvent = require('../systems/loggingSystem');
-module.exports = { name: 'messageDeleteBulk', async execute(client, collection) { const guild = collection.first()?.guild; if (guild) await logEvent(guild, 'Bulk Message Delete', `Deleted ${collection.size} messages.`); } };
